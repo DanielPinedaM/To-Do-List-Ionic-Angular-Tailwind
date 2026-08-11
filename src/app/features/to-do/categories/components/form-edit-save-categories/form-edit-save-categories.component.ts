77a0171de49a7be05fe9c05016c76148db61dcfe
DanielPinedaM <pineda.daniel700@gmail.com>
2026-08-11 15:08:05 -0500

    feat: :construction: crear formulario para editar y guardar categorias

--- src/app/features/to-do/categories/components/form-edit-save-categories/form-edit-save-categories.component.ts
+++ src/app/features/to-do/categories/components/form-edit-save-categories/form-edit-save-categories.component.ts
@@ -1,7 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import {
+  IonButton,
+  IonInput,
+  IonItem,
+  IonList,
+} from "@ionic/angular/standalone";
+
+const MIN_DESCRIPTION_LENGTH = 3;
 
 @Component({
   selector: "app-form-edit-save-categories",
   templateUrl: "./form-edit-save-categories.component.html",
+  imports: [ReactiveFormsModule, IonButton, IonInput, IonItem, IonList],
 })
-export class FormEditSaveCategoriesComponent {}
+export class FormEditSaveCategoriesComponent {
+  private formBuilder = inject(FormBuilder);
+
+  categoryForm = this.formBuilder.nonNullable.group({
+    description: [
+      "",
+      [Validators.required, Validators.minLength(MIN_DESCRIPTION_LENGTH)],
+    ],
+  });
+
+  descriptionControl = this.categoryForm.controls.description;
+
+  submitted = signal(false);
+
+  minDescriptionLength = MIN_DESCRIPTION_LENGTH;
+
+  onSubmitSaveCategory(): void {}
+}
