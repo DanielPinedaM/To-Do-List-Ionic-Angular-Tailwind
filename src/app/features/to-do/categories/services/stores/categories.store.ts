45fafb61d64628b159dd22ef40e83c8d3ec5d1e2
DanielPinedaM <pineda.daniel700@gmail.com>
2026-08-11 14:27:36 -0500

    feat: :sparkles: crear estados globales con signals para categorias y tareas

--- src/app/features/to-do/categories/services/stores/categories.store.ts
+++ src/app/features/to-do/categories/services/stores/categories.store.ts
@@ -1,4 +1,13 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
+import { Categories } from "@/app/features/to-do/categories/interfaces/categories.interface";
 
 @Injectable({ providedIn: "root" })
-export class CategoriesStore {}
+export class CategoriesStore {
+  private categories = signal<Categories[]>([]);
+
+  setCategories(categories: Categories[]): void {
+    this.categories.set(categories);
+  }
+
+  getCategories = (): Categories[] => this.categories();
+}
--- src/app/features/to-do/tasks/services/stores/tasks.store.ts
+++ src/app/features/to-do/tasks/services/stores/tasks.store.ts
@@ -1,4 +1,13 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
+import { Task } from "@/app/features/to-do/tasks/interfaces/tasks.interface";
 
 @Injectable({ providedIn: "root" })
-export class TasksStore {}
+export class TasksStore {
+  private tasks = signal<Task[]>([]);
+
+  setTasks(tasks: Task[]): void {
+    this.tasks.set(tasks);
+  }
+
+  getTasks = (): Task[] => this.tasks();
+}
