f7c955a2c1057b191e61f4a8a8143419c6e184ae
DanielPinedaM <pineda.daniel700@gmail.com>
2026-08-11 17:23:06 -0500

    feat: :sparkles: agregar servicios para CRUD de categorias y tareas

--- src/app/features/to-do/categories/services/categories-crud.service.ts
+++ src/app/features/to-do/categories/services/categories-crud.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
+import { Categories } from "@/app/features/to-do/categories/interfaces/categories.interface";
+import { CategoriesStore } from "@/app/features/to-do/categories/services/stores/categories.store";
+import { CapacitorPreferencesHelperService } from "@/app/features/to-do/services/helpers/capacitor-preferences.helper.service";
+import { ToDoIdGeneratorHelperService } from "@/app/features/to-do/services/helpers/to-do-id-generator.helper.service";
+import { TasksStore } from "@/app/features/to-do/tasks/services/stores/tasks.store";
 
 @Injectable({ providedIn: "root" })
-export class CategoriesCrudService {}
+export class CategoriesCrudService {
+  private categoriesStore = inject(CategoriesStore);
+  private tasksStore = inject(TasksStore);
+  private preferencesHelper = inject(CapacitorPreferencesHelperService);
+  private idGeneratorHelper = inject(ToDoIdGeneratorHelperService);
+
+  async CreateCategory(description: string): Promise<void> {
+    const newCategory: Categories = {
+      id: this.idGeneratorHelper.generateNextCategoryId(),
+      description,
+    };
+
+    const categoriesWithNewCategory: Categories[] = this.categoriesStore
+      .getCategories()
+      .concat(newCategory);
+
+    this.categoriesStore.setCategories(categoriesWithNewCategory);
+    await this.preferencesHelper.saveToPreferences(
+      "categories",
+      categoriesWithNewCategory,
+    );
+  }
+
+  async UpdateCategory(id: string, description: string): Promise<void> {
+    const allCategories: Categories[] = this.categoriesStore
+      .getCategories()
+      .map((category) =>
+        category.id === id ? { ...category, description } : category,
+      );
+
+    this.categoriesStore.setCategories(allCategories);
+    await this.preferencesHelper.saveToPreferences("categories", allCategories);
+  }
+
+  async DeleteById(id: string): Promise<boolean> {
+    const hasTasks: boolean = this.tasksStore
+      .getTasks()
+      .some((task) => task.category === id);
+
+    if (hasTasks) return false;
+
+    const categoriesWithoutDeletedCategory: Categories[] = this.categoriesStore
+      .getCategories()
+      .filter((category) => category.id !== id);
+
+    this.categoriesStore.setCategories(categoriesWithoutDeletedCategory);
+    await this.preferencesHelper.saveToPreferences(
+      "categories",
+      categoriesWithoutDeletedCategory,
+    );
+
+    return true;
+  }
+}
--- src/app/features/to-do/tasks/services/task-crud.service.ts
+++ src/app/features/to-do/tasks/services/task-crud.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from "@angular/core";
+import { Injectable, inject } from "@angular/core";
+import { CapacitorPreferencesHelperService } from "@/app/features/to-do/services/helpers/capacitor-preferences.helper.service";
+import { ToDoIdGeneratorHelperService } from "@/app/features/to-do/services/helpers/to-do-id-generator.helper.service";
+import { Task } from "@/app/features/to-do/tasks/interfaces/tasks.interface";
+import { TasksStore } from "@/app/features/to-do/tasks/services/stores/tasks.store";
 
 @Injectable({ providedIn: "root" })
-export class TaskCrudService {}
+export class TaskCrudService {
+  private tasksStore = inject(TasksStore);
+  private preferencesHelper = inject(CapacitorPreferencesHelperService);
+  private idGeneratorHelper = inject(ToDoIdGeneratorHelperService);
+
+  async CreateTask(task: Omit<Task, "id">): Promise<void> {
+    const newTask: Task = {
+      ...task,
+      id: this.idGeneratorHelper.generateNextTasktId(),
+    };
+
+    const tasksWithNewTask: Task[] = this.tasksStore.getTasks().concat(newTask);
+
+    this.tasksStore.setTasks(tasksWithNewTask);
+    await this.preferencesHelper.saveToPreferences("tasks", tasksWithNewTask);
+  }
+
+  async UpdateTask(
+    id: string,
+    changes: Partial<Omit<Task, "id">>,
+  ): Promise<void> {
+    const allTasks: Task[] = this.tasksStore
+      .getTasks()
+      .map((task) => (task.id === id ? { ...task, ...changes } : task));
+
+    this.tasksStore.setTasks(allTasks);
+    await this.preferencesHelper.saveToPreferences("tasks", allTasks);
+  }
+
+  async DeleteById(id: string): Promise<void> {
+    const tasksWithoutDeletedTask: Task[] = this.tasksStore
+      .getTasks()
+      .filter((task) => task.id !== id);
+
+    this.tasksStore.setTasks(tasksWithoutDeletedTask);
+    await this.preferencesHelper.saveToPreferences(
+      "tasks",
+      tasksWithoutDeletedTask,
+    );
+  }
+}
